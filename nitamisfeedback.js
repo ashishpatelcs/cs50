1c951ecfda88eb2cf1b5c466f8687637504f7c39
Ashish Patel <ashish.tripleclicks@gmail.com>
2017-04-05 13:06:04 +0530

    Create nitamisfeedback.js

---
+++ nitamisfeedback.js
@@ -0,0 +1,4 @@
+/* Author: Ashish Patel
+ * NITA MIS Feedback auto submission
+ */
+$(document).ready(function() { for (var i = 0; i < 11; i++) { $('#ctl00_ContentPlaceHolder1_lvCourse_ctrl'+i+'_rblCourse_4').attr('checked', true); $('#ctl00_ContentPlaceHolder1_lvTeacher_ctrl'+i+'_rblTeacher_4').attr('checked', true); } $('#ctl00_ContentPlaceHolder1_btnSubmit').click(); });
